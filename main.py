41c5e733106734356430f40f565b1511a9d2e4f6
vrshaa66 <varshakk2005@gmail.com>
2026-01-11 12:36:57 +0530

    added highscore feature

--- main.py
+++ main.py
@@ -35,13 +35,13 @@ while game_on:
         score.increase_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
         # if segment == snake.head():
         #     pass
         if snake.head.distance(segment) < 10:
-            game_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,13 @@
 from turtle import Turtle
 ALIGNMENT = "center"
-FONT = ("Arial", 24, "normal")
+FONT = ("Arial", 20, "normal")
 
 class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open('data.txt' , mode = 'r') as file:
+            self.highscore = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 265)
@@ -13,12 +15,20 @@ class Score(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"score: {self.score}", align=ALIGNMENT, font=FONT)
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game over", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} HighScore: {self.highscore}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open('data.txt' , mode ='w') as file:
+                file.write(f"{self.highscore}")
+        self.score = 0
+        self.update_score()
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("Game over", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score+=1
-        self.clear()
         self.update_score()
